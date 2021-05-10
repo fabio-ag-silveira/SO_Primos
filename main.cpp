f256bd8f5e9593ae38b7c14f3b4a72b5ddba5bb4
Lucas Matheus de  Moraes Florentino <51215618+Lucas-M-florentino@users.noreply.github.com>
2021-05-10 17:59:51 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 #include <pthread.h>
 #include <math.h>
 #include <time.h>
+#include <semaphore.h>
 
+sem_t mutex;
 
 using namespace std;
 
 int **vetor,
     THREAD = 5, // VALOR PREDEFINIDO, PARA CASO O USUÁRIO DIGITAR ERRADO.
     TAM = 5000,
-    quantidade_primos = 0; // CONTADOR PARA OS PRIMOS.
+    quantidade_primos = 0,
+    sequencial = 0; // CONTADOR PARA OS PRIMOS.
 
 typedef struct
 {
@@ -23,11 +26,11 @@ typedef struct
 // IDENTIFICA OS NUMEROS PRIMOS.
 bool eh_primo(int num)
 {
-    int raiz_primo = (int)sqrt((float)num);
+    int raiz_primo = sqrt(num);
 
     for (int i=2; i< raiz_primo;  i++)
     {
-        if(num % raiz_primo == 0 || num % i == 0)
+        if( num % i == 0)
         {
             return false;
         }
@@ -44,20 +47,25 @@ void *conta_primos(void *parametro)
     {
         for(int j=vetor_p->inicial_col; j< vetor_p->final_col; j++)
         {
-            if(eh_primo(vetor[i][j]) == true)
+            if(eh_primo(vetor[i][j]))
             {
                 // CONTA OS NUMEROS PRIMOS.
+                if(sequencial == 0){
                 quantidade_primos+=1;
+                }else{
+                    sem_wait(&mutex);
+                    quantidade_primos+=1;
+                    sem_post(&mutex);
+                }
             }
         }
     }
 
     printf("Terminado thread.\n");
-
+    sequencial = 1;
     // FINALIZA AS THREADS.
     pthread_exit((void*) NULL);
 
-    return(vetor_p);
 }
 
 int main ()
@@ -69,8 +77,11 @@ int main ()
     printf("\n2. Definir o numero de Threads (Threads > 1): \n");
     scanf("%d", &THREAD);
 
-    time_t inicial, final;
+    time_t inicial_1,inicial_2, final_1,final_2;
     double t_1, t_2;
+
+    // inicia semaforo
+    sem_init(&mutex, 0, 1);
 
     // ID DA THREAD.
     pthread_t tid[THREAD+1];
@@ -96,11 +107,11 @@ int main ()
     {
         for(int j=0; j< TAM; j++)
         {
-            vetor[i][j] = rand()%100;
+            vetor[i][j] = rand()%(9999999);
         }
     }
 
-    printf("\n\nVerificando numeros primos...\n\n");
+    printf("\n\n\nVerificando numeros primos...\n\n");
 
     // INICIA AS THREADS.
     for(int i=0; i<= THREAD; i++)
@@ -109,7 +120,7 @@ int main ()
     }
 
     // INICIO DO TIME SEQUENCIAL.
-    inicial = clock();
+    inicial_1 = time(0);
 
     // MODO SEQUENCIAL.
     parametro[THREAD].inicial_l = 0;
@@ -117,15 +128,17 @@ int main ()
     parametro[THREAD].inicial_col = 0;
     parametro[THREAD].final_col = TAM;
     pthread_create(&tid[THREAD], &attr[THREAD], conta_primos, &parametro[THREAD]);
+
     pthread_join(tid[THREAD], NULL);
 
-    final = clock();
-    t_1 = ((double)(final-inicial))/(CLOCKS_PER_SEC);
+    final_1 = time(0);
+    t_1 = (final_1-inicial_1);
     quantidade_primos_1 = quantidade_primos;
-    quantidade_primos = 0;
+    quantidade_primos=0;
 
     // INICIO DO TIME.
-    inicial = clock();
+    inicial_2 = time(0);
+
 
     // DELIMITA O INICIO E O FIM DE CADA THREAD.
     for(int i=1; i <= THREAD;  i++)
@@ -134,6 +147,7 @@ int main ()
         parametro[i-1].final_l = (i-1)*(TAM/THREAD)+(TAM/THREAD);
         parametro[i-1].inicial_col = 0;
         parametro[i-1].final_col = TAM;
+
         pthread_create(&tid[i-1], &attr[i-1], conta_primos, &parametro[i-1]);
     }
 
@@ -143,14 +157,14 @@ int main ()
         pthread_join(tid[i], NULL);
     }
 
-    final = clock();
-    t_2 = ((double)(final-inicial))/(CLOCKS_PER_SEC);
+    final_2 = time(0);
+    t_2 = ((final_2-inicial_2));
     quantidade_primos_2 = quantidade_primos;
 
-    printf("\n(Sequencial)\nQuantidade de numeros primos = %d", quantidade_primos);
+    printf("\n(Sequencial)\nQuantidade de numeros primos = %d", quantidade_primos_1);
     printf("\nTempo de execucao: %.4lf s\n\n", t_1);
 
-    printf("\n(Paralelo)\nQuantidade de numeros primos = %d", quantidade_primos);
+    printf("\n(Paralelo)\nQuantidade de numeros primos = %d", quantidade_primos_2);
     printf("\nTempo de execucao: %.4lf s\n\n\n\n", t_2);
 
     // DESALOCA MEMORIA.
@@ -158,6 +172,6 @@ int main ()
     {
         delete vetor[i];
     }
-
+    sem_destroy(&mutex);
     delete vetor;
 }
